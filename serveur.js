c50e835379a51bc6bb6515eacd5bdb6411f317f5
nolliug <jean@guillon.com>
2019-05-31 12:44:31 +0200

    j envoi des trucs qui servent a rien en fait

--- serveur.js
+++ serveur.js
@@ -1,17 +1,19 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<!DOCTYPE html>'+
-'<html>'+
-'    <head>'+
-'        <meta charset="utf-8" />'+
-'        <title>Ma page Node.js !</title>'+
-'    </head>'+ 
-'    <body>'+
-'     	<p>Je veux un hamster a <strong>Noel</strong> !</p>'+
-'    </body>'+
-'</html>');
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if (page == '/') {
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/sous-sol') {
+        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/etage/1/chambre') {
+        res.write('Hé ho, c\'est privé ici !');
+    }
     res.end();
 });
 server.listen(8080);
