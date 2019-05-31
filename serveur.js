f61fc50bb4d1a173f153ffa50c9256e82ff9280b
nolliug <jean@guillon.com>
2019-05-31 12:21:30 +0200

    envoide plus de html

--- serveur.js
+++ serveur.js
@@ -2,6 +2,16 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
+    res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Je veux un hamster a <strong>Noel</strong> !</p>'+
+'    </body>'+
+'</html>');
+    res.end();
 });
 server.listen(8080);
